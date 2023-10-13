cf8d387287196ad2733b253931bf6685e90df0e9
Francisco <salva@morean.co>
2023-10-13 00:22:08 -0300

    improve success payment failed card

--- src/pages/PaymentFailed.js
+++ src/pages/PaymentFailed.js
@@ -1,6 +1,5 @@
-import React from 'react';
 import { motion } from 'framer-motion';
-import { RiCloseFill } from 'react-icons/ri';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PaymentFailure from '../imgs/paymentFailure.svg';
 
@@ -11,22 +10,35 @@ const PaymentFailed = () => {
 	};
 
 	return (
-		<div className='flex items-center justify-center flex-col mt-10 lg:mt-0 dark:bg-gray-900'>
-			<motion.div initial='hidden' animate='visible' variants={failureVariants}>
-				<div className='flex items-center mb-4'>
-					<h2 className='text-red-500 text-2xl font-semibold mr-2'>
-						Pago de Suscripción Fallido
-					</h2>
-					<RiCloseFill className='text-red-500 text-2xl' />
+		<div className='flex items-center justify-center mt-10 p-4 lg:mt-0 dark:bg-gray-900'>
+			<motion.div
+				initial='hidden'
+				animate='visible'
+				variants={failureVariants}
+				className='max-w-3xl w-full p-4 bg-white dark:bg-gray-800 rounded-md shadow-md flex flex-col sm:flex-row mt-4'
+			>
+				<div className='sm:w-1/2 p-4 flex flex-col'>
+					<div className='flex items-center mb-4'>
+						<h2 className='text-red-500 text-2xl font-semibold mr-2'>
+							Pago de Suscripción Fallido
+						</h2>
+					</div>
+					<p className='text-lg dark:text-indigo-400'>
+						¡Inténtelo de nuevo yendo a la página de suscripción!
+					</p>
+					<Link to='/subscripcion'>
+						<button className='mt-4 text-white bg-indigo-500 hover:bg-indigo-600 px-4 py-2 rounded'>
+							Volver a intentar
+						</button>
+					</Link>
 				</div>
-				<p className='text-lg dark:text-indigo-400'>
-					¡Inténtelo de nuevo yendo a la página de suscripción!
-				</p>
-				<Link to='/subscripcion'>
-					<button className='mt-4 text-white bg-indigo-500 hover:bg-indigo-600 px-4 py-2 rounded'>
-						Volver a intentar
-					</button>
-				</Link>
+				<div className='sm:w-1/2 p-4 flex items-center justify-center'>
+					<img
+						src={PaymentFailure}
+						alt='Payment Failure'
+						className='w-48 h-48 sm:w-64 sm:h-64'
+					/>
+				</div>
 			</motion.div>
 		</div>
 	);
